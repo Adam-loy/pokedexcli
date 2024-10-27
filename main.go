0ce9701d1bb0fa4fcbd703c6b7b824e3298b3c7a
Adam-loy <BesbesAdam@proton.me>
2024-10-27 10:46:36 +0000

    Completed

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-  pokeClient := pokeapi.NewClient(5 * time.Second, time.Minute*5)
+  pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
   cfg := &config{
-      pokeapiClient: pokeClient,
+    pokeapiClient: pokeClient,
+    caughtPokemon: map[string]pokeapi.Pokemon{},
   }
 	startRepl(cfg)
 }

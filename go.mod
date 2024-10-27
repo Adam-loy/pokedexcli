fd2b7dde142084a520dcc93c624fe5a71628fa35
Adam-loy <BesbesAdam@proton.me>
2024-10-27 09:46:32 +0000

    fixing issue

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/adam-loy/pokedexcli
+module github.com/Adam-loy/pokedexcli
 
 go 1.23.2
--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -12,34 +12,36 @@ func TestAddGet(t *testing.T) {
     key string
     val []byte
   }{
-    key: "https://example.com",
-    val: []byte("testdata"),
-  },
-  {
-    key: "https://example.com/path",
-    val: []byte("moretestdata"),
-  },
-}
+    {
+      key: "https://example.com",
+      val: []byte("testdata"),
+    },
+    {
+      key: "https://example.com/path",
+      val: []byte("moretestdata"),
+    },
+  }
 
-for i, c := range cases {
-  t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T){
-    cache := NewCache(interval)
-    cache.Add(c.key, c.val)
-    val, ok := cache.Get(c.key)
-    if !ok {
-      t.Errorf("expected to find key")
-      return
-    }
-    if string(val) != string(c.val) {
-      t.Errorf("expected to find value")
-      return
-    }
-  })
+  for i, c := range cases {
+    t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+      cache := NewCache(interval)
+      cache.Add(c.key, c.val)
+      val, ok := cache.Get(c.key)
+      if !ok {
+        t.Errorf("expected to find key")
+        return
+      }
+      if string(val) != string(c.val) {
+        t.Errorf("expected to find value")
+        return
+      }
+    })
+  }
 }
 
 func TestReapLoop(t *testing.T) {
   const baseTime = 5 * time.Millisecond
-  const waitTime = baseTime + 5 * time.Millisecond
+  const waitTime = baseTime + 5*time.Millisecond
   cache := NewCache(baseTime)
   cache.Add("https://example.com", []byte("testdata"))
 
@@ -54,6 +56,6 @@ func TestReapLoop(t *testing.T) {
   _, ok = cache.Get("https://example.com")
   if ok {
     t.Errorf("expected to not find key")
-    return 
+    return
   }
 }
